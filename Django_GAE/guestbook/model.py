18992a54516350e072cd087f85d0de922e00e9d1
theanh90 <anhthe@MacBook-Pro.local>
2015-02-08 00:15:37 +0700

    Fix the code regarding to anh Phuc's comment

--- Django_GAE/guestbook/model.py
+++ Django_GAE/guestbook/model.py
@@ -1,14 +1,12 @@
 from google.appengine.ext import ndb
+from google.appengine.api import memcache
+import logging
 
 DEFAULT_GUESTBOOK_NAME = 'de_name'
 
 # We set a parent key on the 'Greetings' to ensure that they are all in the same
 # entity group. Queries across the single entity group will be consistent.
 # However, the write rate should be limited to ~1/second.
-
-def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
-    '''Constructs a Datastore key for a Guestbook entity with guestbook_name.'''
-    return ndb.Key('Book_key', guestbook_name)
 
 class Author(ndb.Model):
     """Sub model for representing an author."""
@@ -20,3 +18,21 @@ class Greeting(ndb.Model):
     author = ndb.StructuredProperty(Author)
     content = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
+
+    def guestbook_key(self, guestbook_name=DEFAULT_GUESTBOOK_NAME):
+        '''Constructs a Datastore key for a Guestbook entity with guestbook_name.'''
+        return ndb.Key(Greeting, guestbook_name)
+
+    #Use Ggoogle Api Memcache for caching query result.
+    def get_list(self, guestbook_name, count):
+        greetings = memcache.get('%s:greetings' % guestbook_name)
+        if greetings:
+            return greetings
+        else: #query from Datastore
+            greetings_query = Greeting.query(ancestor=self.guestbook_key(guestbook_name)).order(-Greeting.date)
+            greetings = greetings_query.fetch(count)
+            #add greeting to memcache
+            if greetings:
+                if not memcache.set(guestbook_name, greetings, count):
+                    logging.error('Memcache set failed.')
+        return greetings
--- Django_GAE/guestbook/view.py
+++ Django_GAE/guestbook/view.py
@@ -1,15 +1,12 @@
 from django.views.generic.base import TemplateView
-from django.http import HttpResponseRedirect
 from django import forms
 from django.views.generic.edit import FormView
 
 from google.appengine.api import users
-from google.appengine.api import memcache
 
-from guestbook.model import Greeting, Author, guestbook_key, DEFAULT_GUESTBOOK_NAME
+from guestbook.model import Greeting, Author, DEFAULT_GUESTBOOK_NAME
 
 import urllib
-import logging
 
 
 class MainView(TemplateView):
@@ -20,18 +17,7 @@ class MainView(TemplateView):
 
         guestbook_name = self.request.GET.get("guestbook_name", DEFAULT_GUESTBOOK_NAME)
 
-        #Use Ggoogle Api Memcache for caching query result.
-        greetings = memcache.get('%s:greetings' % guestbook_name)
-        if greetings is not None:
-            return greetings
-        else: #query from Datastore
-            greetings_query = Greeting.query(
-                ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-            greetings = greetings_query.fetch(10)
-            #add greeting to memcache
-            if greetings:
-                if not memcache.add(guestbook_name, greetings, 10):
-                    logging.error('Memcache set failed.')
+        greetings = Greeting().get_list(guestbook_name, 10)
 
         current_user = users.get_current_user()
         if current_user:
@@ -47,18 +33,26 @@ class MainView(TemplateView):
         context['guestbook_name'] = guestbook_name
         context['url'] = url
         context['url_linktext'] = url_linktext
-
         return context
 
 class SignForm(forms.Form):
     greeting_message = forms.CharField(label= 'Greeting message', widget=forms.Textarea, required=True, max_length=100 )
     book_name = forms.CharField(label='Guestbook name', max_length=10, required=True)
 
-
 class SignView(FormView):
+
+    def getCleaned_data(self, form):
+        guestbook_name = form.cleaned_data['book_name']
+        content = form.cleaned_data['greeting_message']
+        data =  {'name' : guestbook_name, 'content' : content }
+        return data
 
     template_name = 'guestbook/sign.html'
     form_class = SignForm
+
+    def get_success_url(self):
+        guestbook_name = self.request.POST.get('book_name', DEFAULT_GUESTBOOK_NAME)
+        return ('/map/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
 
     def get_initial(self):
         guestbook_name = self.request.GET.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
@@ -67,17 +61,14 @@ class SignView(FormView):
         return initial
 
     def form_valid(self, form):
-        if self.request.method == 'POST':
-            guestbook_name = self.request.POST.get('book_name', DEFAULT_GUESTBOOK_NAME)
-            greeting = Greeting(parent=guestbook_key(guestbook_name))
-            if users.get_current_user():
-                greeting.author = Author(
-                        identity=users.get_current_user().user_id(),
-                        email=users.get_current_user().email())
+        guestbook_name = self.getCleaned_data(form)['name']
+        greeting = Greeting(parent=Greeting().guestbook_key(guestbook_name))
+        if users.get_current_user():
+            greeting.author = Author(
+                    identity=users.get_current_user().user_id(),
+                    email=users.get_current_user().email())
 
-            greeting.content = self.request.POST.get('greeting_message')
+        greeting.content = self.getCleaned_data(form)['content']
+        greeting.put()
+        return super(SignView, self).form_valid(form)
 
-            greeting.put()
-
-            return HttpResponseRedirect('/map/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
-            return super(SignView, self).form_valid(form)
